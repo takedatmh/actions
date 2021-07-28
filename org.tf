49a5996ce0edbdd992dcee80f61d0dc268f9df1a
Tomohiro Takeda <takedatmh@gmail.com>
2021-07-29 03:27:14 +0900

    Add files via upload

---
+++ org.tf
@@ -0,0 +1,18 @@
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com"
+  ]
+  feature_set = "ALL"
+}
+
+resource aws_organizations_organizational_unit ou1{
+  name = "ou1"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+
+resource aws_organizations_account account1 {
+   name  = "account1"
+   email = "takedatmh+sompo3@gmail.com"
+   parent_id = aws_organizations_organizational_unit.ou1.id
+}
