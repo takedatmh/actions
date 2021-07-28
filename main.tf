36e53a54e53e990ac8cd8cbdf828b7f8431550bb
Tomohiro Takeda <takedatmh@gmail.com>
2021-07-29 04:03:20 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,5 @@
 # Terraform configuration
 # ---------------------------------------------
 terraform {
-  required_version = ">=0.13"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+  required_version = ">=0.12"
 }
