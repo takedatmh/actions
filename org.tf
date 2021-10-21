e3f9ee1d5a49d93208962abf683eff1eb1ad8d14
Tomohiro Takeda <takedatmh@gmail.com>
2021-10-21 15:39:57 +0900

    Update org.tf

--- org.tf
+++ org.tf
@@ -1,12 +1,12 @@
-# resource "aws_organizations_organization" "org" {
-#   aws_service_access_principals = [
-#     "cloudtrail.amazonaws.com",
-#     "config.amazonaws.com"
-#   ]
-#   feature_set = "ALL"
-# }
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com"
+  ]
+  feature_set = "ALL"
+}
 
-resource aws_organizations_organizational_unit ou1{
+resource aws_organizations_organizational_unit ou2{
   name = "ou2"
   parent_id = aws_organizations_organization.org.roots[0].id
 }
