9be57bfebb2ca49e6ff40b1d431a9cd26284b341
Tomohiro Takeda <takedatmh@gmail.com>
2021-10-21 15:42:36 +0900

    Update org.tf

--- org.tf
+++ org.tf
@@ -6,14 +6,14 @@
 #   feature_set = "ALL"
 # }
 
-resource aws_organizations_organizational_unit ou2{
-  name = "ou2"
+resource aws_organizations_organizational_unit ou1{
+  name = "ou1"
   parent_id = aws_organizations_organization.org.roots[0].id
 }
 
 resource aws_organizations_account account1 {
-   name  = "account11"
-   email = "tketomo+terraform11@amazon.co.jp"
+   name  = "account12"
+   email = "tketomo+terraform12@amazon.co.jp"
    #parent_id = aws_organizations_organizational_unit.ou1.id
    
 }
